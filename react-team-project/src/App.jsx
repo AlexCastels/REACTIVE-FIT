64aca71ab4ffc9aa861df0a5a10eb0e1bd73c27d
Annagiusi Volpe <annagiusiv@gmail.com>
2024-02-29 15:41:39 +0100

    aggiunto componente giugi form

--- react-team-project/src/App.jsx
+++ react-team-project/src/App.jsx
@@ -1,14 +1,8 @@
-import { FormUsersSignup } from "./FormUsersSignup";
-import { HelloWorld } from "./components/HelloWorld";
-import { Link, Route , Routes } from 'react-router-dom'
+
 
 export function App() {
 	return (
 		<>
-			<Routes>
-				<Route path='/' element={<HelloWorld/>}/>
-				<Route path="/formusers" element={<FormUsersSignup/>}/>
-			</Routes>
 		</>
 	)
 }
