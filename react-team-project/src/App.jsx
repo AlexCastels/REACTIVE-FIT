a8b9b926709de1468dc58d117287635aa590a300
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2024-03-06 17:14:47 +0100

    RouterNotFound

--- react-team-project/src/App.jsx
+++ react-team-project/src/App.jsx
@@ -1,28 +1,33 @@
-import { GeneralContainer } from "./components/GeneralContainer";
-import { CentralContainer } from "./components/CentralContainer";
+// import { GeneralContainer } from "./components/GeneralContainer";
+// import { CentralContainer } from "./components/CentralContainer";
 import { AsideContainer } from "./components/AsideContainer";
 import { Water } from "./benni-component/Water";
-import { ComponentContainer } from "./benni-component/ComponentContainer";
-import Weight from "./benni-component/Weight";
+// import { ComponentContainer } from "./benni-component/ComponentContainer";
+// import Weight from "./benni-component/Weight";
 import { RegistrationInformation } from "./benni-component/RegistrationInformation";
-import { Element } from "./components/Element";
+// import { Element } from "./components/Element";
 import { Sidebar} from "./chiaraRa-components/sidebar";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { Homepage } from "./chiaraRa-components/Homepage";
+import { PageNotFound } from "./chiaraRa-components/PageNotFound";
+
+
 
 export function App() {
   return (
-    <>
-      <GeneralContainer>
-        <AsideContainer></AsideContainer>
-        <CentralContainer>
-          <Element>
-            <Water />
-          </Element>
-          <Element>
-            <Weight />
-          </Element>
-        </CentralContainer>
-        <AsideContainer></AsideContainer>
-      </GeneralContainer>
+   <>
+     <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/dashboard" element={<Sidebar/>}/>
+      <Route path="/calendario" element={<Water/>}/>
+      <Route path="/notizie" element={<AsideContainer/>}/>
+      <Route path="/logout" element={<RegistrationInformation/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+    </Routes>
+    </BrowserRouter>
+   
+  
     </>
   );
 }
