6e11f64f97034c1c825fab19f9430ab1d6d225df
chiara <chiarapaluzzi34@gmail.com>
2024-03-06 19:52:07 +0100

    chiarapalu

--- react-team-project/src/App.jsx
+++ react-team-project/src/App.jsx
@@ -1,8 +1,9 @@
+import FormAccessoPT from "./Chiarap-components/FormAccessoPT";
 import { UserAccessForm } from "./alex-components/UserAccessForm";
 import { ChiSono } from "./benni-component/ChiSono";
 import { RegistrationInformation } from "./benni-component/RegistrationInformation";
 import { RegistrationUser } from "./benni-component/RegistrationUser";
-
+import {Workandsleep} from "./Chiarap-components/WorkandSleep"
 import { Link, Route , Routes } from 'react-router-dom'
 
 export function App() {
@@ -12,7 +13,8 @@ export function App() {
 				<Route path='/' element={<RegistrationInformation/>}/>
 				<Route path='/scegli' element={<ChiSono/>}/>
 				<Route path='/scegli/useraccess' element={<UserAccessForm/>}/>
-				
+				<Route path='/' element={<FormAccessoPT/>}/>
+				<Route path='/' element={<Workandsleep/>}/>
 				<Route path='/scegli/reg' element={<RegistrationUser/>}/>
 			</Routes> 
 			
