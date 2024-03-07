14eb38106f654bf97c1cb9f3c2077f368300d462
Annagiusi Volpe <annagiusiv@gmail.com>
2024-03-07 08:40:34 +0100

    sez giusi

--- react-team-project/src/App.jsx
+++ react-team-project/src/App.jsx
@@ -1,13 +1,9 @@
 
-import { FormUsersSignup } from "./giusi_components/FormUsersSignup";
-import { UserComponents } from "./giusi_components/UserComponent";
 
 export function App() {
 	return (
 		<>
-		<FormUsersSignup/>
-		
-		<UserComponents/>
+	
 		</>
 	)
 }
