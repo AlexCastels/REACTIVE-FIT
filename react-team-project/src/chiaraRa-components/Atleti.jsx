36c35208daec0c1251ca984da559d54007266ecf
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2024-03-13 12:34:20 +0100

    card atleti

--- react-team-project/src/chiaraRa-components/Atleti.jsx
+++ react-team-project/src/chiaraRa-components/Atleti.jsx
@@ -1,45 +1,219 @@
-import './Atleti.css';
+import "./Atleti.css";
 export function Atleti() {
-    return(
-        <> <div className="AtletiInfoContainer">
-            <div className="AtletiInfo">
-                <div className="AtletaCard">
-                    <div className="AtletaCardSinx">
-                        <div className="AtletaCardSinxImg">
-                            <img src="https://t3.ftcdn.net/jpg/00/88/35/60/160_F_88356081_uBwPo3Axmlz4QrodUuwqgyN5v91LhiKS.jpg" alt="Ragazza" />
-                        </div>
-                        <div className="AtletaCardsinxInfo">
-                            <span>Sofia Rossi</span>
-                            <span>Età: 25</span>
-                            <span>Peso: 80kg</span>
-                            <span>Altezza: 169cm</span>
-                            <span>Email: sofiarossi@email.com</span>
-                            <span>Num: 3334455229</span>
-                        </div>
-                    </div>
-                    <div className="AtletaCardDex">
-                        <h5>Prox Appuntamento</h5>
-                        <form action="">
-                            <div>
-                            <p>Protocollo alimentare</p>
-                            <input type="file" id="myFile" name="filename"/>
-                            <p>Scheda allenamenti</p>
-                            <input type="file" id="myFile" name="filename"/>
-                            <input type="submit"/>
-                            </div>
-                            <div>
-                                <p><label htmlFor="">Invia un messaggio:</label></p>
-                                <textarea name="messaggio" id="messaggio" cols="30" rows="2"></textarea>
-                                <input type="submit" value="submit" />
-                            </div>
-
-                        </form>                  
-                        
-                        
-                    </div>
+  return (
+    <>    
+      <div className="AtletiInfoContainer">
+        <div className="AtletiInfo">
+          <div className="AtletaCard">
+            <div className="AtletaCardSinx">
+              <div className="AtletaCardSinxImg">
+                <img
+                  src="https://2.bp.blogspot.com/_DLMbxIoKN3c/Sfmt_MI2jSI/AAAAAAAAUyE/HJXXjzI6jRs/s400/sophia-bush-17.jpg"
+                  alt="Ragazza"
+                />
+                <h2>Sofia Rossi</h2>
+              </div>
+              <div className="AtletaCardsinxInfo">
+                <span>Età: 25</span>
+                <span>Peso: 80kg</span>
+                <span>Altezza: 169cm</span>
+                <span style={{marginTop:'20px'}}>Contatti:</span>
+                <span>sofiarossi@email.com</span>
+                <span>3334455229</span>
                 </div>
+                <h3>Prox Appuntamento</h3>
             </div>
+            <div className="AtletaCardDex">
+              <form action="">
+                <div className="AtletiImportContainer">
+                  <div className="AtletiImportP">            
+                    <p>Protocollo alimentare</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+                  <div className="AtletiImportA">
+                    <p>Scheda allenamenti</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+                <div className="AtletiTextArea">
+                  <p>
+                    <label htmlFor="">Invia un messaggio:</label>
+                  </p>
+                  <textarea
+                    name="messaggio"
+                    id="messaggio"
+                    cols="36"
+                    rows="2"
+                  ></textarea>
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="AtletaCard">
+            <div className="AtletaCardSinx">
+              <div className="AtletaCardSinxImg">
+                <img
+                  src="https://i.pinimg.com/474x/b5/d9/c9/b5d9c94c8bff8d4fa728e13ddf106bc0.jpg"
+                  alt="Ragazz"
+                />
+                <h2>Giovanni Polacco</h2>
+              </div>
+              <div className="AtletaCardsinxInfo">
+                <span>Età: 35</span>
+                <span>Peso: 75kg</span>
+                <span>Altezza: 180cm</span>
+                <span style={{marginTop:'20px'}}>Contatti:</span>
+                <span>giovpolac@email.com</span>
+                <span>3334455230</span>
+                </div>
+                <h3>Prox Appuntamento</h3>
+            </div>
+            <div className="AtletaCardDex">
+              <form action="">
+                <div className="AtletiImportContainer">
+                  <div className="AtletiImportP">            
+                    <p>Protocollo alimentare</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+                  <div className="AtletiImportA">
+                    <p>Scheda allenamenti</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+                <div className="AtletiTextArea">
+                  <p>
+                    <label htmlFor="">Invia un messaggio:</label>
+                  </p>
+                  <textarea
+                    name="messaggio"
+                    id="messaggio"
+                    cols="36"
+                    rows="2"
+                  ></textarea>
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="AtletaCard">
+            <div className="AtletaCardSinx">
+              <div className="AtletaCardSinxImg">
+                <img
+                  src="https://clipart-library.com/images_k/person-silhouette/person-silhouette-18.png"
+                  alt="Ragazz"
+                />
+                <h2>Marco Bianchi</h2>
+              </div>
+              <div className="AtletaCardsinxInfo">
+                <span>Età: 28</span>
+                <span>Peso: 70kg</span>
+                <span>Altezza: 175cm</span>
+                <span style={{marginTop:'20px'}}>Contatti:</span>
+                <span>bianchima@email.com</span>
+                <span>3334455859</span>
+                </div>
+                <h3>Prox Appuntamento</h3>
+            </div>
+            <div className="AtletaCardDex">
+              <form action="">
+                <div className="AtletiImportContainer">
+                  <div className="AtletiImportP">            
+                    <p>Protocollo alimentare</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+                  <div className="AtletiImportA">
+                    <p>Scheda allenamenti</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+                <div className="AtletiTextArea">
+                  <p>
+                    <label htmlFor="">Invia un messaggio:</label>
+                  </p>
+                  <textarea
+                    name="messaggio"
+                    id="messaggio"
+                    cols="36"
+                    rows="2"
+                  ></textarea>
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="AtletaCard">
+            <div className="AtletaCardSinx">
+              <div className="AtletaCardSinxImg">
+                <img
+                  src="https://clipart-library.com/images_k/person-silhouette/person-silhouette-18.png"
+                  alt="Ragazz"
+                />
+                <h2>Alessia Romano</h2>
+              </div>
+              <div className="AtletaCardsinxInfo">
+                <span>Età:32</span>
+                <span>Peso: 60kg</span>
+                <span>Altezza: 165cm</span>
+                <span style={{marginTop:'20px'}}>Contatti:</span>
+                <span>aleromano@email.com</span>
+                <span>3383455229</span>
+                </div>
+                <h3>Prox Appuntamento</h3>
+            </div>
+            <div className="AtletaCardDex">
+              <form action="">
+                <div className="AtletiImportContainer">
+                  <div className="AtletiImportP">            
+                    <p>Protocollo alimentare</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+                  <div className="AtletiImportA">
+                    <p>Scheda allenamenti</p>
+                    <input type="file" id="myFile" name="filename" />
+                  </div>
+
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+                <div className="AtletiTextArea">
+                  <p>
+                    <label htmlFor="">Invia un messaggio:</label>
+                  </p>
+                  <textarea
+                    name="messaggio"
+                    id="messaggio"
+                    cols="36"
+                    rows="2"
+                  ></textarea>
+                  <button className="AtletiBtn" type="submit">
+                    Invia
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
