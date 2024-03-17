0341083616827174c199917dc4a293498d4337e0
benni90100 <micelibenedetto800@gmail.com>
2024-03-17 14:25:53 +0100

    calendar

--- react-team-project/src/benni-component/CalendarPage.jsx
+++ react-team-project/src/benni-component/CalendarPage.jsx
@@ -1,23 +1,30 @@
-import { Calendar, Whisper, Popover, Badge } from 'rsuite';
-import "./CalendarPage.css"
+import { Calendar, Whisper, Popover, Badge } from "rsuite";
+import "./CalendarPage.css";
 
 function getTodoList(date) {
   const day = date.getDate();
 
   switch (day) {
+    case 1:
+    case 3:
+    case 6:
     case 10:
-      return [
-        { time: '10:30 am', title: 'Meeting' },
-        { time: '12:00 pm', title: 'Lunch' }
-      ];
+    case 13:
+    case 15:
+    case 17:
+    case 20:
+    case 24:
+    case 27:
+    case 31:
+      return [{ time: "10:30 am", title: "allenamento" }];
     case 15:
       return [
-        { time: '09:30 pm', title: 'Products Introduction Meeting' },
-        { time: '12:30 pm', title: 'Client entertaining' },
-        { time: '02:00 pm', title: 'Product design discussion' },
-        { time: '05:00 pm', title: 'Product test and acceptance' },
-        { time: '06:30 pm', title: 'Reporting' },
-        { time: '10:00 pm', title: 'Going home to walk the dog' }
+        { time: "09:30 pm", title: "Products Introduction Meeting" },
+        { time: "12:30 pm", title: "Client entertaining" },
+        { time: "02:00 pm", title: "Product design discussion" },
+        { time: "05:00 pm", title: "Product test and acceptance" },
+        { time: "06:30 pm", title: "Reporting" },
+        { time: "10:00 pm", title: "Going home to walk the dog" },
       ];
     default:
       return [];
@@ -68,4 +75,3 @@ export const CalendarPage = () => {
 
   return <Calendar bordered renderCell={renderCell} />;
 };
-
