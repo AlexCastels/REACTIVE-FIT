e14041d886beb9d3016558cbea0ec18ccdd97da4
benni90100 <micelibenedetto800@gmail.com>
2024-03-17 16:48:02 +0100

    tailwindconfig

--- react-team-project/src/components/Element.jsx
+++ react-team-project/src/components/Element.jsx
@@ -1,5 +1,5 @@
 export function Element({children}) {
-    return <div className="rounded-xl border-2 border-gray-800 z-10  backdrop-blur-50"> 
+    return <div className="rounded-xl border-2 border-slate-800 z-10  backdrop-blur-50"> 
     {children}
     </div>
 }
