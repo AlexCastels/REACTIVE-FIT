e4c2e5d10b51b43ded96cd1f8b458cb27ade6651
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-03-26 12:52:18 +0100

    Aggiunta Sezione WorkApi con Gif

--- react-team-project/src/App.jsx
+++ react-team-project/src/App.jsx
@@ -48,7 +48,7 @@ export function App() {
             </Routes>
         </WeightProvider>
       </WaterProvider> */}
-      {/* <WorkList/> */}
+      <WorkList/>
     </>
   );
 }
--- react-team-project/src/alex-components/WorkList.jsx
+++ react-team-project/src/alex-components/WorkList.jsx
@@ -1,30 +1,46 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { WorkApiCard } from "./WorkApiCard";
+import './workApi.css'
 
-// export function WorkList() {
-//     const [data, setData] = useState([]);
 
-//     async function getData() {
-//         const url =
-//             "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=10";
-//         const options = {
-//             method: "GET",
-//             headers: {
-//                 "X-RapidAPI-Key":
-//                     "09d11f3934msh4cb88cb42be833fp1623ebjsn0a4f9c4bce76",
-//                 "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-//             },
-//         };
+export function WorkList() {
+    
+    const [data , setData] = useState([])
+    
+    async function getData() {
+        const url =
+            "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=30";
+        const options = {
+            method: "GET",
+            headers: {
+                "X-RapidAPI-Key":
+                    "09d11f3934msh4cb88cb42be833fp1623ebjsn0a4f9c4bce76",
+                "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+            },
+        };
 
-//         try {
-//             const response = await fetch(url, options);
-//             const result = await response.json();
-//             console.log(result);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            setData(result)
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-//     useEffect(() => {
-//         getData();
-//     }, []);
-// }
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div className="workApi-general-container">
+            <div className="workApi-card-container">
+                {data.map((item) => (
+                    <WorkApiCard key={item.id} item={item} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
