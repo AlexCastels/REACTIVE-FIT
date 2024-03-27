3c57b37b838d03ea3ff0d2d82a596254077a65ff
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-03-27 13:59:19 +0100

    Modificato stile btn Homepage

--- react-team-project/src/alex-components/UserAccessForm.jsx
+++ react-team-project/src/alex-components/UserAccessForm.jsx
@@ -6,47 +6,47 @@ import { FormUsersSignup } from "../giusi_components/FormUsersSignup";
 import { useLogin } from "../context/LoginContext";
 
 export function UserAccessForm() {
-  const [handleInput, handleForm, input, setInput] = useLogin();
-  return (
-    <div className="container-form-homepage">
-      <div className="general-container">
-        <form onSubmit={handleForm} className="form-container">
-          <h3 className="h5-form">Accedi con le tue credenziali</h3>
-          <div className="input">
-            <label htmlFor="email"> E-mail</label>
-            <input
-              className="inputForm"
-              type="text"
-              onChange={handleInput}
-              name="username"
-              value={input.username}
-            />
-          </div>
-          <div className="input">
-            <label htmlFor="password"> Password</label>
-            <input
-              className="inputForm"
-              type="text"
-              onChange={handleInput}
-              name="password"
-              value={input.password}
-            />
-          </div>
-          <div className="container-buttons-form">
-            <button type="submit">Invia</button>
-            <Link to={"/iscriviti"} element={<FormUsersSignup />}>
-              <ButtonComponent text={"Sign-up"} />
-            </Link>
-          </div>
-          <div className="container-button-pt">
-            <h5>Sei un Personal Trainer?</h5>
-            <div className="container-button-pt-iscriviti">
-              <ButtonComponent text={"Accedi"} />
-              <p>o iscriviti</p>
+    const [handleInput, handleForm, input, setInput] = useLogin();
+    return (
+        <div className="container-form-homepage">
+            <div className="general-container">
+                <form onSubmit={handleForm} className="form-container">
+                    <h3 className="h5-form">Accedi con le tue credenziali</h3>
+                    <div className="input">
+                        <label htmlFor="email"> E-mail</label>
+                        <input
+                            className="inputForm"
+                            type="text"
+                            onChange={handleInput}
+                            name="username"
+                            value={input.username}
+                        />
+                    </div>
+                    <div className="input">
+                        <label htmlFor="password"> Password</label>
+                        <input
+                            className="inputForm"
+                            type="text"
+                            onChange={handleInput}
+                            name="password"
+                            value={input.password}
+                        />
+                    </div>
+                    <div className="container-buttons-form">
+                        <button type="submit" className="bg-yellow-400 hover:bg-transparent h-9 px-5 ml-4 py-2 text-sm shadow-sm hover:shadow-lg font-medium hover:text-gray-50 text-slate-950 rounded-md transition ease-in duration-700 max-w-40">Invia</button>
+                        <Link to={"/iscriviti"} element={<FormUsersSignup />}>
+                            <ButtonComponent text={"Sign-up"} />
+                        </Link>
+                    </div>
+                    <div className="container-button-pt">
+                        <h5>Sei un Personal Trainer?</h5>
+                        <div className="container-button-pt-iscriviti">
+                            <ButtonComponent text={"Accedi"} />
+                            <p>o iscriviti</p>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
